88c09896476e82678a1fa49cf22a2f29a6689871
unknown <student@server03>
2025-11-26 20:05:49 +0500

    create каркас programm

--- vitekvladimir.cpp
+++ vitekvladimir.cpp
@@ -1,9 +1,42 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Функция 1: Чтение строк из файла в вектор
+vector<string> readLinesFromFile(const string& filename) {
+    vector<string> lines;
+    
+    return lines;
 }
 
+// Функция 2: Вывод строк на экран
+void printLinesToConsole(const vector<string>& lines) {
+    
+}
+
+// Функция 3: Запись строк в файл
+void writeLinesToFile(const vector<string>& lines, const string& filename) {
+    
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Программа для работы с текстовыми файлами" << endl;
+
+    // Функция 1: Чтение из файла
+    vector<string> lines = readLinesFromFile("input.txt");
+
+    // Функция 2: Вывод на экран
+    printLinesToConsole(lines);
+
+    // Функция 3: Запись в файл
+    writeLinesToFile(lines, "output.txt");
+
+    cout << "Программа завершена" << endl;
+    return 0;
+}
